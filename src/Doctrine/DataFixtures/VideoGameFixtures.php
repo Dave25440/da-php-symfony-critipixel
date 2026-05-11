ca4deb7196d880b86f979c0b80bf2f06ac538817
David Horès <hores.d@gmail.com>
2026-05-11 12:10:43 +0200

    Update VideoGameFixtures to add tags and reviews to video games

--- src/Doctrine/DataFixtures/VideoGameFixtures.php
+++ src/Doctrine/DataFixtures/VideoGameFixtures.php
@@ -2,10 +2,13 @@
 
 namespace App\Doctrine\DataFixtures;
 
+use App\Model\Entity\Review;
+use App\Model\Entity\Tag;
 use App\Model\Entity\User;
 use App\Model\Entity\VideoGame;
 use App\Rating\CalculateAverageRating;
 use App\Rating\CountRatingsPerValue;
+use DateInterval;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -20,35 +23,61 @@ final class VideoGameFixtures extends Fixture implements DependentFixtureInterfa
         private readonly Generator $faker,
         private readonly CalculateAverageRating $calculateAverageRating,
         private readonly CountRatingsPerValue $countRatingsPerValue
-    ) {
-    }
+    ) {}
 
     public function load(ObjectManager $manager): void
     {
-        $users = $manager->getRepository(User::class)->findAll();
+        $tags = $manager->getRepository(Tag::class)->findAll();
+        $users = array_chunk($manager->getRepository(User::class)->findAll(), 5);
 
         $videoGames = array_fill_callback(0, 50, fn (int $index): VideoGame => (new VideoGame)
             ->setTitle(sprintf('Jeu vidéo %d', $index))
             ->setDescription($this->faker->paragraphs(10, true))
-            ->setReleaseDate(new DateTimeImmutable())
+            ->setReleaseDate((new DateTimeImmutable())->sub(new DateInterval(sprintf('P%dD', $index))))
             ->setTest($this->faker->paragraphs(6, true))
             ->setRating(($index % 5) + 1)
             ->setImageName(sprintf('video_game_%d.png', $index))
             ->setImageSize(2_098_872)
         );
 
-        // TODO : Ajouter les tags aux vidéos
+        array_walk($videoGames, static function (VideoGame $videoGame, int $index) use ($tags) {
+            for ($tagIndex = 0; $tagIndex < 5; $tagIndex++) {
+                $videoGame->getTags()->add($tags[($index + $tagIndex) % count($tags)]);
+            }
+        });
 
         array_walk($videoGames, [$manager, 'persist']);
 
         $manager->flush();
 
-        // TODO : Ajouter des reviews aux vidéos
+        array_walk($videoGames, function (VideoGame $videoGame, int $index) use ($users, $manager) {
+            $reviewers = $users[$index % 5];
 
+            foreach ($reviewers as $user) {
+                $review = (new Review())
+                    ->setUser($user)
+                    ->setVideoGame($videoGame)
+                    ->setRating($this->faker->numberBetween(1, 5))
+                    ->setComment($this->faker->paragraphs(1, true))
+                ;
+
+                $videoGame->getReviews()->add($review);
+
+                $manager->persist($review);
+
+                $this->calculateAverageRating->calculateAverage($videoGame);
+                $this->countRatingsPerValue->countRatingsPerValue($videoGame);
+            }
+        });
+
+        $manager->flush();
     }
 
     public function getDependencies(): array
     {
-        return [UserFixtures::class];
+        return [
+            TagFixtures::class,
+            UserFixtures::class
+        ];
     }
 }
