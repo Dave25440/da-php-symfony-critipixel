fb4ca34942ffaa36bcab4f3ed4580e9e908952cb
David Horès <hores.d@gmail.com>
2026-05-18 18:45:56 +0200

    Create testCountRatingsPerValueWithMultipleReviews method, ratingsProvider and increaseRatings method in CountRatingsPerValueTest

--- tests/Unit/CountRatingsPerValueTest.php
+++ tests/Unit/CountRatingsPerValueTest.php
@@ -21,6 +21,64 @@ final class CountRatingsPerValueTest extends TestCase
         $this->ratingHandler = new RatingHandler();
     }
 
+    private static function increaseRatings(int $one = 0, int $two = 0, int $three = 0, int $four = 0, int $five = 0): NumberOfRatingPerValue
+    {
+        $numberOfRatingPerValue = new NumberOfRatingPerValue();
+
+        for ($i = 0; $i < $one; $i++) {
+            $numberOfRatingPerValue->increaseOne();
+        }
+
+        for ($i = 0; $i < $two; $i++) {
+            $numberOfRatingPerValue->increaseTwo();
+        }
+
+        for ($i = 0; $i < $three; $i++) {
+            $numberOfRatingPerValue->increaseThree();
+        }
+
+        for ($i = 0; $i < $four; $i++) {
+            $numberOfRatingPerValue->increaseFour();
+        }
+
+        for ($i = 0; $i < $five; $i++) {
+            $numberOfRatingPerValue->increaseFive();
+        }
+
+        return $numberOfRatingPerValue;
+    }
+
+    /**
+     * @return iterable<array{NumberOfRatingPerValue, int, int...}>
+     */
+    public function ratingsProvider(): iterable
+    {
+        yield '2 reviews rated 1' => [
+            self::increaseRatings(2),
+            1, 1,
+        ];
+
+        yield '3 reviews rated 1, 2 and 3' => [
+            self::increaseRatings(1, 1, 1),
+            1, 2, 3,
+        ];
+
+        yield '5 reviews with all possible ratings' => [
+            self::increaseRatings(1, 1, 1, 1, 1),
+            1, 2, 3, 4, 5,
+        ];
+
+        yield '3 reviews rated 3, 4 and 5' => [
+            self::increaseRatings(0, 0, 1, 1, 1),
+            3, 4, 5,
+        ];
+
+        yield '2 reviews rated 3 and 4 and 2 reviews rated 5' => [
+            self::increaseRatings(0, 0, 1, 1, 2),
+            3, 4, 5, 5,
+        ];
+    }
+
     public function testCountRatingsPerValueWithNoReview(): void
     {
         $this->ratingHandler->countRatingsPerValue($this->videoGame);
@@ -34,5 +92,19 @@ final class CountRatingsPerValueTest extends TestCase
         $this->ratingHandler->countRatingsPerValue($this->videoGame);
 
         $this->assertSame(1, $this->videoGame->getNumberOfRatingsPerValue()->getNumberOfFive());
+    }
+
+    /**
+     * @dataProvider ratingsProvider
+     */
+    public function testCountRatingsPerValueWithMultipleReviews(NumberOfRatingPerValue $expectedNumberOfRatingPerValue, int ...$ratings): void
+    {
+        foreach ($ratings as $rating) {
+            $this->videoGame->getReviews()->add((new Review())->setRating($rating));
+        }
+
+        $this->ratingHandler->countRatingsPerValue($this->videoGame);
+
+        $this->assertEquals($expectedNumberOfRatingPerValue, $this->videoGame->getNumberOfRatingsPerValue());
     }
 }
