1ccdccef987076e5b374d334d5da44e874b3a2c5
David Horès <hores.d@gmail.com>
2026-05-18 17:22:51 +0200

    Create testCountRatingsPerValueWithOneReview method in CountRatingsPerValueTest

--- tests/Unit/CountRatingsPerValueTest.php
+++ tests/Unit/CountRatingsPerValueTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Unit;
 
 use App\Model\Entity\NumberOfRatingPerValue;
+use App\Model\Entity\Review;
 use App\Model\Entity\VideoGame;
 use App\Rating\RatingHandler;
 use PHPUnit\Framework\TestCase;
@@ -25,5 +26,13 @@ final class CountRatingsPerValueTest extends TestCase
         $this->ratingHandler->countRatingsPerValue($this->videoGame);
 
         $this->assertEquals(new NumberOfRatingPerValue(), $this->videoGame->getNumberOfRatingsPerValue());
+    }
+
+    public function testCountRatingsPerValueWithOneReview(): void
+    {
+        $this->videoGame->getReviews()->add((new Review())->setRating(5));
+        $this->ratingHandler->countRatingsPerValue($this->videoGame);
+
+        $this->assertSame(1, $this->videoGame->getNumberOfRatingsPerValue()->getNumberOfFive());
     }
 }
