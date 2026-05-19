c3840582336b19fb3788ff411a83289820005d69
David Horès <hores.d@gmail.com>
2026-05-19 15:08:19 +0200

    Create setUp and testPostReview methods in ReviewTest

--- tests/Functional/VideoGame/ReviewTest.php
+++ tests/Functional/VideoGame/ReviewTest.php
@@ -1,17 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Functional\VideoGame;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Response;
 
-class ReviewTest extends WebTestCase
+final class ReviewTest extends WebTestCase
 {
-    public function testSomething(): void
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+    private KernelBrowser $client;
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testPostReview(): void
+    {
+        $crawler = $this->client->request('GET', '/jeu-video-0');
+
+        $form = $crawler->selectButton('Poster')->form();
+        $form['review[rating]'] = 5;
+        $form['review[comment]'] = 'Jeu vidéo 0 est mon jeu de rôle préféré.';
+
+        $this->client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $this->client->followRedirect();
     }
 }
