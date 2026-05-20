606d4b48a769d4ed6f138b0eb198b5b492b99395
David Horès <hores.d@gmail.com>
2026-05-20 14:27:30 +0200

    Create testFilterVideoGamesByTags method in FilterTest

--- tests/Functional/VideoGame/FilterTest.php
+++ tests/Functional/VideoGame/FilterTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Functional\VideoGame;
 
 use App\Tests\Functional\FunctionalTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 final class FilterTest extends FunctionalTestCase
 {
@@ -25,5 +26,25 @@ final class FilterTest extends FunctionalTestCase
         $this->client->submitForm('Filtrer', ['filter[search]' => 'Jeu vidéo 49'], 'GET');
         self::assertResponseIsSuccessful();
         self::assertSelectorCount(1, 'article.game-card');
+    }
+
+    public function testFilterVideoGamesByTags(): void
+    {
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorCount(10, 'article.card.game-card');
+        $this->assertSelectorTextSame('article.card.game-card:first-child span.tag:first-child', 'Tag 0');
+
+        $form = $crawler->selectButton('Filtrer')->form();
+        $form['filter[tags][0]'] = 1;
+
+        $this->assertSame('GET', $form->getMethod());
+        $this->client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorCount(10, 'article.card.game-card');
+        $this->assertSelectorTextSame('article.card.game-card:first-child span.tag:first-child', 'Tag 0');
+        $this->assertSelectorTextSame('article.card.game-card:last-child span.tag:first-child', 'Tag 0');
     }
 }
