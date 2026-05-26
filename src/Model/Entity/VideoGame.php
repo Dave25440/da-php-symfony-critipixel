30d08d38a49d4b303b6d9f5ef676d5465cfd03ca
David Horès <hores.d@gmail.com>
2026-05-26 13:58:13 +0200

    Update VideoGame Entity

--- src/Model/Entity/VideoGame.php
+++ src/Model/Entity/VideoGame.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Model\Entity;
 
 use DateTimeImmutable;
-use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
